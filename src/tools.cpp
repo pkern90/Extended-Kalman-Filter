380df595ce1f3c66913178be49487e7e0e3daf85
pkern90 <pkern90@gmail.com>
2017-03-05 13:44:40 +0100

    feat: add rmse calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -7,10 +7,24 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+
+    if(estimations.size() == 0 || estimations.size() != ground_truth.size()){
+        cout << "Error: invalid input values." << endl;
+        return rmse;
+    }
+
+    for(int i=0; i < estimations.size(); ++i){
+        VectorXd res = estimations[i] - ground_truth[i];
+        res = res.array() * res.array();
+        rmse += res;
+    }
+
+    rmse /= estimations.size();
+    rmse = rmse.array().sqrt();
+
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
