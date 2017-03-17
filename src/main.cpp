2c6012aa501652d07eb68b1de22ad3f1df06c748
pkern90 <pkern90@gmail.com>
2017-03-17 10:58:46 +0100

    refactor: performance optimization

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ bool useOnlyLidar = false;
 string in_file_name_ = "";
 string out_file_name_ = "";
 
-void parseOptions(int argc, char *argv[]){
+void parseOptions(int argc, char *argv[]) {
     try {
         cxxopts::Options options(argv[0], " - Implementation of an extended Kalman filter to"
                 " fuse lidar and radar sensor data.\n"
@@ -61,6 +61,7 @@ void parseOptions(int argc, char *argv[]){
     }
 }
 
+
 void check_files(ifstream &in_file, string &in_name,
                  ofstream &out_file, string &out_name) {
     if (!in_file.is_open()) {
@@ -74,121 +75,131 @@ void check_files(ifstream &in_file, string &in_name,
     }
 }
 
-void readData(ifstream &in_file_, vector<MeasurementPackage> &measurement_pack_list,
-              vector<GroundTruthPackage> &gt_pack_list) {
-    string line;
 
-    // prep the measurement packages (each line represents a measurement at a timestamp)
-    while (getline(in_file_, line)) {
+tuple<MeasurementPackage, GroundTruthPackage> parseLine(string line) {
+    istringstream iss(line);
+    string sensor_type;
+    MeasurementPackage meas_package;
+    GroundTruthPackage gt_package;
+    long timestamp;
 
-        istringstream iss(line);
-        string sensor_type;
-        MeasurementPackage meas_package;
-        GroundTruthPackage gt_package;
-        long timestamp;
-
-        // reads first element from the current line
-        iss >> sensor_type;
-        if (sensor_type.compare("L") == 0) {
-            // LASER MEASUREMENT
-            if(useOnlyRadar){
-                continue;
-            }
-
-            // read measurements at this timestamp
-            meas_package.sensor_type_ = MeasurementPackage::LASER;
-            meas_package.raw_measurements_ = VectorXd(2);
-            float x;
-            float y;
-            iss >> x;
-            iss >> y;
-            meas_package.raw_measurements_ << x, y;
-            iss >> timestamp;
-            meas_package.timestamp_ = timestamp;
-            measurement_pack_list.push_back(meas_package);
-        } else if (sensor_type.compare("R") == 0) {
-            // RADAR MEASUREMENT
-            if(useOnlyLidar){
-                continue;
-            }
-
-            // read measurements at this timestamp
-            meas_package.sensor_type_ = MeasurementPackage::RADAR;
-            meas_package.raw_measurements_ = VectorXd(3);
-            float ro;
-            float theta;
-            float ro_dot;
-            iss >> ro;
-            iss >> theta;
-            iss >> ro_dot;
-            meas_package.raw_measurements_ << ro, theta, ro_dot;
-            iss >> timestamp;
-            meas_package.timestamp_ = timestamp;
-            measurement_pack_list.push_back(meas_package);
-        }
-
-        // read ground truth data to compare later
-        float x_gt;
-        float y_gt;
-        float vx_gt;
-        float vy_gt;
-        iss >> x_gt;
-        iss >> y_gt;
-        iss >> vx_gt;
-        iss >> vy_gt;
-        gt_package.gt_values_ = VectorXd(4);
-        gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
-        gt_pack_list.push_back(gt_package);
+    // reads first element from the current line
+    iss >> sensor_type;
+    if (sensor_type.compare("L") == 0) {
+        // LASER MEASUREMENT
+        // read measurements at this timestamp
+        meas_package.sensor_type_ = MeasurementPackage::LASER;
+        gt_package.sensor_type_ = GroundTruthPackage::LASER;
+        meas_package.raw_measurements_ = VectorXd(2);
+        float x;
+        float y;
+        iss >> x;
+        iss >> y;
+        meas_package.raw_measurements_ << x, y;
+        iss >> timestamp;
+        meas_package.timestamp_ = timestamp;
+    } else if (sensor_type.compare("R") == 0) {
+        // RADAR MEASUREMENT
+        // read measurements at this timestamp
+        meas_package.sensor_type_ = MeasurementPackage::RADAR;
+        gt_package.sensor_type_ = GroundTruthPackage::RADAR;
+        meas_package.raw_measurements_ = VectorXd(3);
+        float ro;
+        float theta;
+        float ro_dot;
+        iss >> ro;
+        iss >> theta;
+        iss >> ro_dot;
+        meas_package.raw_measurements_ << ro, theta, ro_dot;
+        iss >> timestamp;
+        meas_package.timestamp_ = timestamp;
     }
+
+    // read ground truth data to compare later
+    float x_gt;
+    float y_gt;
+    float vx_gt;
+    float vy_gt;
+    iss >> x_gt;
+    iss >> y_gt;
+    iss >> vx_gt;
+    iss >> vy_gt;
+    gt_package.timestamp_ = timestamp;
+    gt_package.gt_values_ = VectorXd(4);
+    gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
+
+    return make_tuple(meas_package, gt_package);
 }
 
-void processData(ofstream &out_file_, const vector<MeasurementPackage> &measurement_pack_list,
-                 const vector<GroundTruthPackage> &gt_pack_list, vector<VectorXd> &estimations,
-                 vector<VectorXd> &ground_truth) {
 
-    FusionEKF fusionEKF;
+void writeLine(ofstream &out_file_, FusionEKF &fusionEKF,
+               MeasurementPackage meas_package, GroundTruthPackage gt_package) {
+    // output the estimation
+    out_file_ << fusionEKF.ekf_.x_(0) << "\t";
+    out_file_ << fusionEKF.ekf_.x_(1) << "\t";
+    out_file_ << fusionEKF.ekf_.x_(2) << "\t";
+    out_file_ << fusionEKF.ekf_.x_(3) << "\t";
 
-    //Call the EKF-based fusion
-    size_t N = measurement_pack_list.size();
-    for (size_t k = 0; k < N; ++k) {
-        // start filtering from the second frame (the speed is unknown in the first frame)
-        fusionEKF.ProcessMeasurement(measurement_pack_list[k]);
-
-        // output the estimation
-        out_file_ << fusionEKF.ekf_.x_(0) << "\t";
-        out_file_ << fusionEKF.ekf_.x_(1) << "\t";
-        out_file_ << fusionEKF.ekf_.x_(2) << "\t";
-        out_file_ << fusionEKF.ekf_.x_(3) << "\t";
-
+    // output the measurements
+    if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
         // output the measurements
-        if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::LASER) {
-            // output the measurements
-            out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
-            out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
-        } else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR) {
-            // output the measurements in the cartesian coordinates
-            double rho = measurement_pack_list[k].raw_measurements_(0);
-            double phi = measurement_pack_list[k].raw_measurements_(1);
-            out_file_ << rho * cos(phi) << "\t";
-            out_file_ << rho * sin(phi) << "\t";
+        out_file_ << meas_package.raw_measurements_(0) << "\t";
+        out_file_ << meas_package.raw_measurements_(1) << "\t";
+    } else if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+        // output the measurements in the cartesian coordinates
+        double rho = meas_package.raw_measurements_(0);
+        double phi = meas_package.raw_measurements_(1);
+        out_file_ << rho * cos(phi) << "\t";
+        out_file_ << rho * sin(phi) << "\t";
+    }
+
+    // output the ground truth packages
+    out_file_ << gt_package.gt_values_(0) << "\t";
+    out_file_ << gt_package.gt_values_(1) << "\t";
+    out_file_ << gt_package.gt_values_(2) << "\t";
+    out_file_ << gt_package.gt_values_(3) << "\n";
+}
+
+
+void processStream(ifstream &in_file_, ofstream &out_file_) {
+    FusionEKF fusionEKF;
+    vector<VectorXd> estimations;
+    vector<VectorXd> ground_truth;
+    string line;
+    int cnt = 0;
+    while (getline(in_file_, line)) {
+        if (line.empty()) {
+            continue;
         }
 
-        // output the ground truth packages
-        out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
-        out_file_ << gt_pack_list[k].gt_values_(1) << "\t";
-        out_file_ << gt_pack_list[k].gt_values_(2) << "\t";
-        out_file_ << gt_pack_list[k].gt_values_(3) << "\n";
+        auto lineParsed = parseLine(line);
+        auto meas_package = get<0>(lineParsed);
+        auto gt_package = get<1>(lineParsed);
+
+        if (useOnlyRadar & meas_package.sensor_type_ == MeasurementPackage::LASER) {
+            continue;
+        } else if (useOnlyLidar & meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+            continue;
+        }
+
+        fusionEKF.ProcessMeasurement(meas_package);
 
         estimations.push_back(fusionEKF.ekf_.x_);
-        ground_truth.push_back(gt_pack_list[k].gt_values_);
+        ground_truth.push_back(gt_package.gt_values_);
 
-        if(verbose){
-            cout << "***** Entry: " << (k + 1) << " *****\n" << endl;
+        writeLine(out_file_, fusionEKF, meas_package, gt_package);
+
+        if (verbose) {
+            cout << "***** Entry: " << ++cnt << " *****\n" << endl;
             cout << "x_ = " << fusionEKF.ekf_.x_ << "\n" << endl;
             cout << "P_ = " << fusionEKF.ekf_.P_ << "\n" << endl;
         }
     }
+
+    // compute the accuracy (RMSE)
+    cout << "Accuracy - RMSE:" << endl << tools::CalculateRMSE(estimations, ground_truth) << endl;
 }
+
 
 int main(int argc, char *argv[]) {
     parseOptions(argc, argv);
@@ -198,17 +209,7 @@ int main(int argc, char *argv[]) {
 
     check_files(in_file_, in_file_name_, out_file_, out_file_name_);
 
-    vector<MeasurementPackage> measurement_pack_list;
-    vector<GroundTruthPackage> gt_pack_list;
-    readData(in_file_, measurement_pack_list, gt_pack_list);
-
-    // used to compute the RMSE later
-    vector<VectorXd> estimations;
-    vector<VectorXd> ground_truth;
-    processData(out_file_, measurement_pack_list, gt_pack_list, estimations, ground_truth);
-
-    // compute the accuracy (RMSE)
-    cout << "Accuracy - RMSE:" << endl << tools::CalculateRMSE(estimations, ground_truth) << endl;
+    processStream(in_file_, out_file_);
 
     // close files
     if (out_file_.is_open()) {
